82562a64a5e17e37e6c37bd10fd5e098fcb738c2
tsunakit99 <tainosuke0926@gmail.com>
2025-04-03 02:43:22 +0900

    add: todos/のindex追加

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,7 +1,22 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Todos') }}
-        </h2>
-    </x-slot>
-</x-app-layout>
+<!-- resources/views/todos/index.blade.php -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Todo一覧</title>
+</head>
+<body>
+    <h1>Todo一覧</h1>
+
+    <ul>
+        @forelse ($todos as $todo)
+            <li>
+                {{ $todo->title }} - 
+                {{ $todo->done ? '✅ 完了' : '⏳ 未完了' }}
+            </li>
+        @empty
+            <li>データがありません</li>
+        @endforelse
+    </ul>
+</body>
+</html>
