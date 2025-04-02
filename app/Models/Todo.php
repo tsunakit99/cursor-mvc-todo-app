dad982fed905630341d67fd21bdb88e99c635b28
tsunakit99 <tainosuke0926@gmail.com>
2025-04-03 02:12:38 +0900

    add: boolean型のアクセサとミューテータを定義

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $fillable = ['title', 'completed'];
+
+    public function getCompletedAttribute($value)
+    {
+        return $value ? true : false;
+    }
+
+    public function setCompletedAttribute($value)
+    {
+        $this->attributes['completed'] = $value ? true : false;
+    }
+    
+
+    
+    
 }
